fc6a8b0cc92e061820fd0e9a1f53e3d0ce0f398b
GABAPENTIN <adius729@gmail.com>
2025-11-15 18:10:58 +0300

    KTS-1-5 исправление на отображение двузначных цифр

--- app/src/main/kotlin/Lesson1_5.kt
+++ app/src/main/kotlin/Lesson1_5.kt
@@ -7,5 +7,5 @@ fun main() {
     val minutes = (totalSeconds % 3600) / 60
     val seconds = totalSeconds % 60
 
-    println("$hours:$minutes:$seconds")
+    println("${"%02d".format(hours)}:%02d:%02d".format(minutes, seconds))
 }
