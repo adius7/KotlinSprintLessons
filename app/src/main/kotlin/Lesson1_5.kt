42d49e156a22b1b082e42c6d52ed95950efd4f35
GABAPENTIN <adius729@gmail.com>
2025-11-15 18:03:33 +0300

    KTS-1-5 Переводим секунды в часы и минуты

---
+++ app/src/main/kotlin/Lesson1_5.kt
@@ -0,0 +1,11 @@
+package org.example.app
+
+fun main() {
+    val totalSeconds = 6480
+
+    val hours = totalSeconds / 3600
+    val minutes = (totalSeconds % 3600) / 60
+    val seconds = totalSeconds % 60
+
+    println("$hours:$minutes:$seconds")
+}
